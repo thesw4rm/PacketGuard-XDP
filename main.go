0f4c6446384d97187fd4e7fc65e82f2f0084ae37
Yadunandan Pillai <ytpillai@pm.me>
2023-10-09 19:31:47 -0400

    Made stuff work
    Signed-off-by: Yadunandan Pillai <ytpillai@pm.me>

---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"C"
+	"fmt"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+	log "github.com/sirupsen/logrus"
+)
+
+import (
+	"net"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/cilium/ebpf/rlimit"
+)
+
+const (
+	TYPE_ENTER = 1
+	TYPE_DROP  = 2
+	TYPE_PASS  = 3
+)
+
+func main() {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	// Remove MEMLOCK resource limit
+	if err := rlimit.RemoveMemlock(); err != nil {
+		// Handle error
+		fmt.Printf("Failed to remove MEMLOCK limit: %v\n", err)
+	}
+
+	if len(os.Args) < 2 {
+		fmt.Println("please provide the network interface")
+		return
+	}
+
+	iface := os.Args[1]
+
+	spec, err := ebpf.LoadCollectionSpec("main.bpf.o")
+	if err != nil {
+		panic(err)
+	}
+	coll, _ := ebpf.NewCollection(spec)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create new collection: %v\n", err))
+	}
+	if err != nil {
+		panic(fmt.Sprintf("Failed to update map: %v\n", err))
+	}
+	defer coll.Close()
+	prog := coll.Programs["drop_packets"]
+	if prog == nil {
+		panic("No program named 'capture_packets' found in collection")
+	}
+
+	iface_idx, err := net.InterfaceByName(iface)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to get interface %s: %v\n", iface, err))
+	}
+	opts := link.XDPOptions{
+		Program:   prog,
+		Interface: iface_idx.Index,
+		// Flags is one of XDPAttachFlags (optional).
+	}
+	lnk, err := link.AttachXDP(opts)
+	if err != nil {
+		panic(err)
+	}
+	
+	var lookupKey uint32 = 0
+	var portNum uint16 = 4040
+	portMap, ok := coll.Maps["port"]
+	if !ok {
+		panic("No map named 'port'")
+	}
+	err = portMap.Update(lookupKey, portNum, ebpf.UpdateAny)
+	if err != nil {
+		panic(fmt.Sprintf("Could not update port map %v", err))
+	}
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+	var packets_dropped []uint16
+	for range ticker.C {
+		err := coll.Maps["rxcnt"].Lookup(lookupKey, &packets_dropped)
+		if err != nil {
+			panic(fmt.Sprintf("Could not lookup rxcnt %v", err))
+		}
+		var total_dropped uint16 = 0
+		for _, cpuvalue := range packets_dropped {
+			total_dropped += cpuvalue
+		}
+
+		log.Info("Dropped packets:", total_dropped)
+	}
+	fmt.Println("Successfully loaded and attached BPF program.")
+	fmt.Println("Press Ctrl+c to end detach the program")
+	defer lnk.Close()
+
+	<-sig
+	
+}
